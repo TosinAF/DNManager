f60682ca5783158f351efa6cc0a000f2fef6f9b3
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-05-07 15:38:52 +0100

    Initial Commit

---
+++ DNManagerClient/DNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DNAppDelegate.h
+//  DNManagerClient
+//
+//  Created by Tosin Afolabi on 07/05/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DNManagerClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DNManagerClient
+//
+//  Created by Tosin Afolabi on 07/05/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DNAppDelegate class]));
+    }
+}
